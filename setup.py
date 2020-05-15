30bfe3fc885b603de294112949d0bcb3c2d4cde9
Patrick Jenkins <me@patjenk.com>
2020-05-15 19:41:59 +0000

    Adding a setup.py so we can install via pip.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="dummydb",
+    version="0.0.1",
+    description="An inefficient and error prone database written in python for prototyping python projects.",
+    url="https://github.com/patjenk/dummydb",
+    author="Patrick Jenkins",
+    author_email="me@patjenk.com",
+    license="",
+    packages=["dummydb",],
+    zip_save=False
+)
